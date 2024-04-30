6dec477409d4d250572b20b3a7bbb1b88282016b
L <99180485+OhradkaGergo@users.noreply.github.com>
2024-04-30 14:11:39 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function KivalasztottKo ()
+{
+    let kepKo = document.getElementById("kepKo");
+    kepKo.style.border = "2px red solid"
+}
+
+function KivalasztottPapir ()
+{
+    let kepPapir = document.getElementById("kepPapir");
+    kepPapir.style.border = "2px red solid"
+}
+
+function KivalasztottOllo ()
+{
+    let kepOllo = document.getElementById("kepOllo");
+    kepOllo.style.border = "2px red solid"
+}
